c91d1c70da72c2fb99f2aa0a0ecefa41403ef505
masudRana88 <mahimuddin88@gmail.com>
2021-11-18 21:35:06 +0600

    dotenv is add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,9 +10,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://oda_car:EvTovHi2ExitXrAp@cluster0.zgxio.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zgxio.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+console.log(uri)
 
 async function run() {
   try {
